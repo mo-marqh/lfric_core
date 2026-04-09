f2f751e47c5fda907f036f934787d11b60d903b5
mo-marqh <87382490+mo-marqh@users.noreply.github.com>
2026-04-09 10:59:22 +0000

    version string

---
+++ infrastructure/source/utilities/lfric_core_version_mod.F90
@@ -0,0 +1,15 @@
+!-----------------------------------------------------------------------------
+! (C) Crown copyright 2026 Met Office. All rights reserved.
+! The file LICENCE, distributed with this code, contains details of the terms
+! under which the code may be used.
+!-----------------------------------------------------------------------------
+!> @brief Module containing the lfric_core version string
+!>
+
+MODULE lfric_core_version_mod
+
+IMPLICIT NONE
+
+CHARACTER(LEN=*), PARAMETER :: lfric_core_version_char = 'vn3.1.1_dev'
+
+END MODULE lfric_core_version_mod
