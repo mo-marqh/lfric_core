02c35644111596f588d4d83cb4024966d1a5e4e2
mo-marqh <87382490+mo-marqh@users.noreply.github.com>
2026-07-14 08:15:56 +0000

    cylc test configuration & code quality linting

--- rose-stem/app/validate_iodef/bin/test_validate_iodef_xml.py
+++ rose-stem/app/validate_iodef/bin/test_validate_iodef_xml.py
@@ -1,14 +1,33 @@
+#!/usr/bin/env python3
+##############################################################################
+# (c) Crown copyright Met Office. All rights reserved.
+# The file LICENCE, distributed with this code, contains details of the terms
+# under which the code may be used.
+##############################################################################
+"""
+XIOS Iodef.xml metadata consistency validation tests.
+"""
+
 import glob
 import os
+import re
 import xml.etree.ElementTree as ET
 
 import pytest
 
 
-def srcReplace(parent, path):
+# security pattern to check whether `src` links are local and link to known
+# controlled facets of the local source tree.  Do not load from unknown sources.
+# allows strings that start with:
+# 'metadata/' etc/' './' '../metadata' '$SOURCE_ROOT/'
+# followed by alphabetic characters only
+security_re = re.compile(r'^(metadata/|etc/|\./|../metadata/|\$SOURCE_ROOT/)'
+                         r'[a-zA-Z]+\w')
+
+def src_replace(parent, path):
     """
-    Recursively update input parent element, resolVing `src` content and replacing this
-    with parsed XML elements from referenced files.
+    Recursively update input parent element, resolVing `src` content and
+    replacing this with parsed XML elements from referenced files.
     """
     i = 0
     for elem in parent:
@@ -18,31 +37,32 @@ def srcReplace(parent, path):
             newelem = ET.parse(inf).getroot()
             parent[i] = newelem
         else:
-            srcReplace(elem, path)
+            src_replace(elem, path)
         i += 1
 
 
 def load_source_xml(fname):
+    """
+    Load a source XML file and recursively populated `src` links from
+    identified safe local paths.
+    """
     path = os.path.dirname(fname)
-    tree = ET.parse(fname)
-    root = tree.getroot()
+    load_tree = ET.parse(fname)
+    load_root = load_tree.getroot()
 
-    # Validate that external elements, defined using `src=` are only accessing known internal paths
-    # to minimise rish from "XML external entity attack"
-    for elem in root.findall('.//*[@src]'):
-        if not (elem.attrib['src'].startswith('metadata/') or
-                elem.attrib['src'].startswith('etc/') or
-                elem.attrib['src'].startswith('./') or
-                elem.attrib['src'].startswith('../') or
-                elem.attrib['src'].startswith('$SOURCE_ROOT/')):
-            raise ValueError('only `src` attributes from local `metadata/` or '
+    # Validate that external elements, defined using `src=` are only accessing
+    # known internal paths to minimise rish from "XML external entity attack"
+    for elem in load_root.findall('.//*[@src]'):
+        if security_re.match(elem.attrib['src']) is None:
+            raise ValueError('only `src` attributes from local, `metadata/`, '
                              '`etc/` or `$SOURCE_ROOT/`paths are supported, not '
                              f'{elem.attrib["src"]}')
-    srcReplace(root, path)
-    return tree
+    src_replace(load_root, path)
+    return load_tree
 
-root_dir = os.environ.get('$SOURCE_ROOT', '/home/users/mark.hedley/metofficegit/') + 'lfric_core'
-
+# Generator for the pytest parametrize fixture.
+root_dir = os.environ.get('SOURCE_PATH', '')
+print(f'root_dir = {root_dir}')
 iodef_likes = []
 for iodef_like in glob.glob('**/iodef*.xml',
                             root_dir=root_dir,
@@ -52,25 +72,29 @@ for iodef_like in glob.glob('**/iodef*.xml',
     root = tree.getroot()
     iodef_likes.append((root, infile))
 
-# tree = load_source_xml(os.path.join(hard_path, iodef))
-# root = tree.getroot()
+if len(iodef_likes) == 0:
+    raise ValueError('No `iodef*.xml` files found.')
 
-@pytest.mark.parametrize("aroot, infile", iodef_likes)
-def test_unique_field_ids_within_context(aroot, infile):
+@pytest.mark.parametrize("aroot, test_file", iodef_likes)
+def test_unique_field_ids_within_context(aroot, test_file):
+    """
+    Pytest test to verify that `field` element `id`s are unique within
+    a given XIOS context.
+    """
     print(f'validating XIOS XML for {aroot} ...')
 
     for context in aroot.findall('.//context'):
-        field_ids = set()    
+        field_ids = set()
         for elem in context.findall('.//field[@id]'):
-    # for elem in aroot.findall('.//field[@id]'):
             eid = elem.attrib.get('id')
             if eid is not None:
-                err_str = (f'Within {infile}\n:'
+                err_str = (f'Within {test_file}\n:'
                            '\tThe `field` element:\n'
                            f'{elem.attrib}\n'
                            f'\thas an `id`: "{eid}" '
                            'which is already defined for an existing field.\n'
-                           '\tXIOS will not distinguish betwen these fields, operations '
-                           'using this id will affect all fields with this `id`')
+                           '\tXIOS will not distinguish betwen these fields, '
+                           'operations using this id will affect all fields '
+                           'with this `id` leading to indeterminate behaviour')
                 assert eid not in field_ids, err_str
                 field_ids.add(eid)
